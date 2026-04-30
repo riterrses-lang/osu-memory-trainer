727990cf280dc4d4091ade86dca79f1124f90c68
riterrses-lang <riterrses@gmail.com>
2026-04-30 11:43:42 +0300

    Set base path and clarify Web Workers comment
    Updated base path for repository and added comment clarification.

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // ВАЖНО: укажите название вашего репозитория в кавычках!
+  base: '/osu-memory-trainer/', 
   plugins: [react()],
   worker: {
-    format: 'module', // Нужно для наших Web Workers
+    format: 'module', // Для корректной работы Web Workers
   },
 })
